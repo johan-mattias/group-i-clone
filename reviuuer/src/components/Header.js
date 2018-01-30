efc654c28b0aaeab21e7a4d3b114218c517696ca
Linus Frosteryd <linus.frosteryd@gmail.com>
2018-01-30 14:23:05 +0100

    added simple header

--- reviuuer/src/components/Header.js
+++ reviuuer/src/components/Header.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => (
-  <header>
-    <nav>
-      <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/roster'>Course</Link></li>
-        <li><Link to='/schedule'>Teacher</Link></li>
-      </ul>
-    </nav>
-  </header>
-)
+//import '../styles/header.css';
 
-export default Header
+class header extends Component {
+
+  render() {
+    const {
+      authSignIn,
+    } = this.props;
+
+    return (
+      <div className="header-splash">
+        <div className="logo-wrap">
+          <div id="logo"></div>
+        </div>
+          <ul>
+             <li><Link to='/'>Home</Link></li>
+             <li><Link to='/Course'>Course</Link></li>
+             <li><Link to='/Teacher'>Teacher</Link></li>
+             <li><Link to="/Signin">
+                <button id="mobile-btn-sign" type="button"> Sign in </button>
+              </Link></li>
+          </ul>
+      </div>
+    );
+  }
+}
+
+export default header;
