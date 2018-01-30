ed7a29d8f48f116e9f50f6424f48419fbea11496
Linus Frosteryd <linus.frosteryd@gmail.com>
2018-01-30 21:20:52 +0100

    Change how we redirect users and added support to let users access the portal

--- reviuuer/src/components/Home.js
+++ reviuuer/src/components/Home.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {ReactDOM, BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import '../Style/App.css';
+import login from './LoginPage.js'
+import splash from './Splash.js'
+
 
 const Home = () => (
-  <div>
-    <h1>Hello world!</h1>
-  </div>
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">HOME</Link></li>
+        <li><Link to="/login">LOGIN</Link></li>
+      </ul>
+      <Route path="/"  exact component={ splash }/>
+      <Route path="/login" exact component={ login }/>
+    </div>
+  </Router>
 )
 
-export default Home
+export default Home;
--- reviuuer/src/index.js
+++ reviuuer/src/index.js
@@ -1,10 +1,23 @@
-import React from 'react'
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './components/App';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-), document.getElementById('root'));
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Home from './components/Home';
+import Portal from './components/Portal';
+
+import './Style/index.css';
+
+const provider = (
+        <Router>
+            <div>
+                <Switch>
+                    <Route exact path="/" component={ Home }></Route>
+                    <Route path="/portal" component={ Portal }></Route> {/*TODO LÄGG TILL SÅ MAN BLIR REDIRECT OM MAN HAR VALID KEY*/}
+                    <Route component={ Home } />
+                </Switch>
+            </div>
+        </Router>
+);
+
+ReactDOM.render(provider, document.getElementById('root'));
