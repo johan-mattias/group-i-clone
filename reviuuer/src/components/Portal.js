77ce3305710aa77da52ed37ae013c00ad6954bb4
Linus Frosteryd <linus.frosteryd@gmail.com>
2018-02-02 18:11:28 +0100

    Moved the portal to localhost:3000/portal

--- reviuuer/src/components/Portal.js
+++ reviuuer/src/components/Portal.js
@@ -1,1 +1,32 @@
-// HÄR ÄR TÄNKT ATT MAN KOMMER BLI REDIRECT I INLOGGAT LÄGE 
+import React, { Component } from 'react';
+import {ReactDOM, BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import FrontPageButton from './FrontPageButton.js'
+import frontButton from '../Style/frontButton.css'
+import SendButton from './SendButton.js'
+import reviews from './Reviews.js'
+import NavbarButton from './NavbarButton';
+import Footer from './Footer';
+import SignOutButton from './SignOutButton'
+
+class Portal extends React.Component {
+
+  render() {
+    return (
+        <Router>
+          <div>
+            <Link to="/login">
+                <SignOutButton>
+                     Sign Out
+                </SignOutButton>
+            </Link>
+
+            <Footer/>
+            <Route path="/reviews" exact component={ reviews }/>
+              <SendButton>send</SendButton>
+            </div>
+        </Router>
+    );
+  };
+}
+
+export default Portal;
