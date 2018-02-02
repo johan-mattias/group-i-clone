5ea79b0330e007c90c711848f3a29ef943ede227
Tom Althin <tom.althin@gmail.com>
2018-02-02 15:02:50 +0100

    navbarbuttons change bgcolor on click

--- reviuuer/src/components/Footer.js
+++ reviuuer/src/components/Footer.js
@@ -3,27 +3,40 @@ import NavbarButton from './NavbarButton'
 
 class Footer extends Component{
     
-    state = {
-        lightBlue : true 
+
+    state = { 
+        lightBlue : [true, true, true]
     }
 
-    changeColor(){
-        this.setState({lightBlue: !this.state.lightBlue})
-        console.log('CLIKED!')
-        
+    changeColor(number){
+        if(number == 0){
+            this.setState({lightBlue : [false, true, true]})
+        }
+        if(number == 1){
+            this.setState({lightBlue : [true, false, true]})
+        }
+        if(number == 2){
+            this.setState({lightBlue : [true, true, false]})
+        }        
 
     }
     
     render(){
-        let bgColor = {
-            backgroundColor: this.state.lightBlue ? '#2F80ED' : '#3B5E8F'
+        let bgColorHome = {
+            backgroundColor: this.state.lightBlue[0] ? '#2F80ED' : '#3B5E8F'
         }
-        console.log(bgColor)
+        let bgColorCourse = {
+            backgroundColor: this.state.lightBlue[1] ? '#2F80ED' : '#3B5E8F'
+        }
+        let bgColorTeacher = {
+            backgroundColor: this.state.lightBlue[2] ? '#2F80ED' : '#3B5E8F'
+        }
+        
         return(
             <div> 
-            <NavbarButton changeStyle={bgColor} onClick={() => {this.changeColor()}}>Home</NavbarButton>
-            <NavbarButton>Course</NavbarButton>
-            <NavbarButton>Teacher</NavbarButton>
+            <NavbarButton changeStyle={bgColorHome} onClick={() => {this.changeColor(0)}}>Home</NavbarButton>
+            <NavbarButton changeStyle={bgColorCourse} onClick={() => {this.changeColor(1)}}>Course</NavbarButton>
+            <NavbarButton changeStyle={bgColorTeacher} onClick={() => {this.changeColor(2)}}>Teacher</NavbarButton>
                 
             </div>
         )
