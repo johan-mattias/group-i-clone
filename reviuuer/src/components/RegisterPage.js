e33a90d7ceab80959456ee22fdbc534b986df87b
Linus Frosteryd <linus.frosteryd@gmail.com>
2018-02-03 18:02:24 +0100

    Check in the browser so we get correct password and lenght

--- reviuuer/src/components/RegisterPage.js
+++ reviuuer/src/components/RegisterPage.js
@@ -7,7 +7,8 @@ class Register extends Component {
   constructor(props) {
     super(props);
     this.state = {email: '',
-                  password: ''};
+                  password: '',
+                  passwordVerify:'',};
 
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -20,20 +21,36 @@ class Register extends Component {
     this.setState({password: event.target.value});
   }
 
-  handleSubmit(event) { //TODO LÄGGA IN SÅ VI KOLLAR I DB 
-    fetch('/api/reg', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            email: this.state.email,
-            password: this.state.password
-        })
-    })
-
+  PwdClickCheck(event) {
+    this.setState({passwordVerify: event.target.value});
+  }
+  
+  handleSubmit(event) { //TODO LÄGGA IN SÅ VI KOLLAR I DB
     event.preventDefault();
+    var pwd1 = this.state.password
+    var pwd2 = this.state.passwordVerify
+
+    if (pwd1.length < 4){
+      console.log("Password needs to be longer than 4")
+    }
+
+    else if (pwd1 !== pwd2){
+      console.log("Passwords don't match")
+    }
+
+    else{
+      fetch('/api/reg', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+              email: this.state.email,
+              password: this.state.password
+          })
+      })
+    }
   }
 
   render() {
@@ -43,6 +60,8 @@ class Register extends Component {
           <input className="login" placeholder="Email" value={this.state.email} onChange={this.EmailClick.bind(this)} />
           <br></br>
           <input className="login" placeholder="Password" value={this.state.password} onChange={this.PwdClick.bind(this)} />
+          <br></br>
+          <input className="login" placeholder="Password" value={this.state.passwordVerify} onChange={this.PwdClickCheck.bind(this)} />
           <br></br>
           <input className="submit" type="submit" value="LOGIN" />
         </form>
