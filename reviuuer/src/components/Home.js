2a972d42f2df14cdfc2cff8d1a4291fd5137e5c8
Linus Frosteryd <linus.frosteryd@gmail.com>
2018-02-03 18:20:39 +0100

    Fixed the home so it looks better between tabs

--- reviuuer/src/components/Home.js
+++ reviuuer/src/components/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import {ReactDOM, BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import '../Style/App.css';
-import login from './LoginPage.js'
+import login from './LoginPage.js';
 import Register from './RegisterPage';
-import reviews from './Reviews.js'
-import addReview from './AddReviewPage'
+import reviews from './Reviews.js';
+import addReview from './AddReviewPage';
+import splash from './FrontPageButton';
 
 
 class Home extends React.Component {
@@ -12,22 +13,20 @@ class Home extends React.Component {
   render() {
     return (
         <Router>
-          
           <div className="flex-container blueBackground">
-          <button className="home"><Link className="link" to="/register">Sign up</Link></button>
-          <button className="home"><Link className="link" to="/login">Sign in</Link></button>
-            <ul>
-              <li><Link to="/">HOME</Link></li>
-              <li><Link to="/login">LOGIN</Link></li>
-              <li><Link to="/register">REGISTER</Link></li>
-              <li><Link to="/reviews">REVIEWS</Link></li>
-              <li><Link to="/addreview">ADD REVIEWS</Link></li>
-            </ul>
-            <h1>ReviUUer</h1>
-            <Route path="/login" exact component={ login }/>
-            <Route path="/register" exact component={ Register }/>
-            <Route path="/reviews" exact component={ reviews }/>
-            <Route path="/addreview" exact component={ addReview }/>
+            <h1><Link className="link logo" to="/">ReviUUer</Link></h1>
+              <ul>
+                <li><Link to="/">HOME</Link></li>
+                <li><Link to="/login">LOGIN</Link></li>
+                <li><Link to="/register">REGISTER</Link></li>
+                <li><Link to="/reviews">REVIEWS</Link></li>
+                <li><Link to="/addreview">ADD REVIEWS</Link></li>
+              </ul>
+              <Route path="/" exact component={ splash }/>
+              <Route path="/login" exact component={ login }/>
+              <Route path="/register" exact component={ Register }/>
+              <Route path="/reviews" exact component={ reviews }/>
+              <Route path="/addreview" exact component={ addReview }/>
           </div>
         </Router>
     );
