5e82c9d9bca01c0693b0f5090790badcfdf7f0f5
fatrique <lar.patrik@gmail.com>
2018-02-11 10:55:21 +0100

    Renamed the texts and cleaned up the code

--- reviuuer/src/components/Footer.js
+++ reviuuer/src/components/Footer.js
@@ -3,26 +3,22 @@ import { NavLink } from 'react-router-dom'
 import '../Style/Button.css'
 import '../Style/Portal.css'
 
-class Footer extends Component{
-
-    
-  render(){
-
+class Footer extends Component{    
+  render() {
     return(
       <div className = "footer"> 
         <NavLink to="/portal" exact activeClassName="selected" className = "navLink">
           <button className="nav">Home</button>
         </NavLink>
         <NavLink to="/portal/Course" exact activeClassName="selected" className = "navLink" >
-          <button className="nav" >Course</button>
+          <button className="nav" >Courses</button>
         </NavLink>
         <NavLink to="/portal/Teacher" exact activeClassName="selected" className = "navLink" >
-          <button className="nav">Teacher</button>    
+          <button className="nav">Teachers</button>    
         </NavLink>
       </div>
-    )
-
-  }
+    );
+  };
 }
 
 export default Footer;
