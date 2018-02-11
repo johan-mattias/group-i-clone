b63fd08c906fca7c28e8b6af652700e43b0b1def
Linus Frosteryd <linus.frosteryd@gmail.com>
2018-02-11 11:42:26 +0100

    Validating cookies on all sensitiv pages

--- reviuuer/src/components/Course.js
+++ reviuuer/src/components/Course.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import Cookies from "universal-cookie";
 
 import Footer from './Footer';
 import '../Style/Portal.css';
 
 class Course extends React.Component{
+
+  componentWillMount() {
+    const c = new Cookies();
+    var cookieFromUser = c.get('user')
+    var fetchURL = `/api/auth?cookie=${cookieFromUser}`;
+    fetch( fetchURL )
+    .then(
+        (res) => { 
+        if(res.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' +
+            res.status);
+          return;
+        }
+      res.json()
+          .then((json) => { 
+            const access = json.accessCookie
+            console.log(access)
+            if (access === true) {
+               console.log("correct cookie ")
+             }
+            else {
+              console.log("Wrong cookie ")
+              this.props.history.push('/')
+            }
+          })
+        })
+    document.body.classList.remove('home');
+    document.body.classList.add('portal'); //adding the correct background by setting the class of the body
+  }
+
   render() {
     return (
       <div className="portal">
